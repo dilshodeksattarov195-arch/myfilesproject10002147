5e4efe5ad194950226653fc8614736425f96165c
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-27 10:48:13 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class userSalculateWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = userSalculateWorker()
+    print("Worker engine initialized.")
